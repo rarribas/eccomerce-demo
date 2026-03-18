3b1c9eff6cc5eb00e2fae35228838554e86f6a35
Raul Arribas <raul.ap84@gmail.com>
2026-03-18 15:38:21 +0000

    dynamic links to product page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Products } from './features/products/products';
+import { Product } from './features/product/product';
 export const routes: Routes = [
   { path: 'products', component: Products }, 
+  { path: 'products/:id', component: Product },
+
   { path: '', redirectTo: '/products', pathMatch: 'full' }
 ];
--- src/app/shared/components/card/card.ts
+++ src/app/shared/components/card/card.ts
@@ -1,13 +1,15 @@
 import { Component, input, Input } from '@angular/core';
 import { required } from '@angular/forms/signals';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './card.html',
   styleUrl: './card.css',
 })
 export class Card {
+  @Input({required: true}) id: string = '';
   @Input({required: true}) title: string = '';
   @Input({required: true}) miniDescription: string = '';
   @Input({required: true}) price: number = 0;
